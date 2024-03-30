d12828cf35083649c3c63d1fc2378167a54bddc1
Wbq01 <1250284238@qq.com>
2024-03-30 17:04:22 +0800

    第一次提交

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+#include "task.h"
+extern int x;
+void main(void)
+{
+    
+    xxx();
+    while (1)
+    {
+        /* code */
+    }
+    
+
+}
---
+++ task.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+// int x = 10;
+void xxx()
+{
+    printf("1111");
+    
+}
